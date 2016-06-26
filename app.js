dca4dec5ed4282e3d1b74ec7d6bc7727164e428c
Omar Sobh <om.sobh@gmail.com>
2016-06-26 14:36:22 -0700

    added in initial routes

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var connect = require('./routes/users');
 
 var app = express();
 
