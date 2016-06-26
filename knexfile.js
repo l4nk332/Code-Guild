94cb3920a11ec713bba6140064ed085057f542ea
Yeshe Wingerd <yeshito@gmail.com>
2016-06-26 15:52:37 -0700

    users topics tables created

--- knexfile.js
+++ knexfile.js
@@ -4,25 +4,8 @@ module.exports = {
 
   development: {
     client: 'pg',
-    connection: {
-      filename: 'postgres://localhost/code_guild'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'code_guild',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+    connection: 'postgres://localhost:5432/code_guild',
+    debug: true
   },
 
   production: {
