7c2123e30646308b672e5bcfdf367b90ce0b9b3a
Yeshe Wingerd <yeshito@gmail.com>
2016-06-26 18:43:25 -0700

    added topics seed data

--- seeds/02_insert_topics.js
+++ seeds/02_insert_topics.js
@@ -1,15 +1,59 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('topics').del()
     .then(function () {
         // Inserts seed entries
-        knex('topics').insert({title: 'Javascript'})
-        knex('topics').insert({title: 'SQL'})
-        knex('topics').insert({title: 'Java'})
-        knex('topics').insert({title: 'PHP'})
-        knex('topics').insert({title: 'Python'})
-        knex('topics').insert({title: 'Objective-C'})
-        knex('topics').insert({title: 'Ruby'})
-    });
+      return knex('topics').insert({title: 'Javascript'});
+    })
+    .then(function() {
+      return knex('topics').insert({title: 'SQL'});
+    })
+    .then(function() {
+      return knex('topics').insert({title: 'Java'});
+    })
+    .then(function() {
+      return knex('topics').insert({title: 'PHP'});
+    })
+    .then(function() {
+      return knex('topics').insert({title: 'Python'});
+    })
+    .then(function() {
+      return knex('topics').insert({title: 'Objective-C'});
+    })
+    .then(function() {
+      return knex('topics').insert({title: 'C'});
+    })
+    .then(function() {
+      return knex('topics').insert({title: 'C#'});
+    })
+    .then(function() {
+      return knex('topics').insert({title: 'C++'});
+    })
+    .then(function() {
+      return knex('topics').insert({title: 'Ruby'});
+    })
+    .then(function() {
+      return knex('topics').insert({title: 'Swift'});
+    })
+    .then(function() {
+      return knex('topics').insert({title: 'R'});
+    })
+    .then(function() {
+      return knex('topics').insert({title: 'Perl'});
+    })
+    .then(function() {
+      return knex('topics').insert({title: 'MATLAB'});
+    })
+    .then(function() {
+      return knex('topics').insert({title: 'Groovy'});
+    })
+    .then(function() {
+      return knex('topics').insert({title: 'Pascal'});
+    })
+    .then(function() {
+      return knex('topics').insert({title: 'Delphi'});
+    }).catch(function(err) {
+      console.log('there was an error:', err);
+    })
 };
