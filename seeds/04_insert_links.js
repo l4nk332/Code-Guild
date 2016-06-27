6cf9fb8b6c7b629c05078eeed57dad959deb2da7
l4nk332 <l4nk332@gmail.com>
2016-06-26 19:38:16 -0700

    Adds links seed data for the application

--- seeds/04_insert_links.js
+++ seeds/04_insert_links.js
@@ -2,58 +2,58 @@ exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
   return knex('links').del()
     .then(function () {
-        knex('links').insert({'user_id': 1, source: 'http://www.github.com'});
+        return knex('links').insert({'user_id': 1, source: 'http://www.github.com'});
     })
     .then(function () {
-        knex('links').insert({'user_id': 1, source: 'http://www.codewars.com'});
+        return knex('links').insert({'user_id': 1, source: 'http://www.codewars.com'});
     })
     .then(function () {
-        knex('links').insert({'user_id': 1, source: 'http://www.linkedin.com'});
+        return knex('links').insert({'user_id': 1, source: 'http://www.linkedin.com'});
     })
     .then(function () {
-        knex('links').insert({'user_id': 2, source: 'http://www.github.com'});
+        return knex('links').insert({'user_id': 2, source: 'http://www.github.com'});
     })
     .then(function () {
-        knex('links').insert({'user_id': 2, source: 'http://www.codewars.com'});
+        return knex('links').insert({'user_id': 2, source: 'http://www.codewars.com'});
     })
     .then(function () {
-        knex('links').insert({'user_id': 2, source: 'http://www.linkedin.com'});
+        return knex('links').insert({'user_id': 2, source: 'http://www.linkedin.com'});
     })
     .then(function () {
-        knex('links').insert({'user_id': 3, source: 'http://www.github.com'});
+        return knex('links').insert({'user_id': 3, source: 'http://www.github.com'});
     })
     .then(function () {
-        knex('links').insert({'user_id': 3, source: 'http://www.codewars.com'});
+        return knex('links').insert({'user_id': 3, source: 'http://www.codewars.com'});
     })
     .then(function () {
-        knex('links').insert({'user_id': 3, source: 'http://www.linkedin.com'});
+        return knex('links').insert({'user_id': 3, source: 'http://www.linkedin.com'});
     })
     .then(function () {
-        knex('links').insert({'user_id': 4, source: 'http://www.github.com'});
+        return knex('links').insert({'user_id': 4, source: 'http://www.github.com'});
     })
     .then(function () {
-        knex('links').insert({'user_id': 4, source: 'http://www.codewars.com'});
+        return knex('links').insert({'user_id': 4, source: 'http://www.codewars.com'});
     })
     .then(function () {
-        knex('links').insert({'user_id': 4, source: 'http://www.linkedin.com'});
+        return knex('links').insert({'user_id': 4, source: 'http://www.linkedin.com'});
     })
     .then(function () {
-        knex('links').insert({'user_id': 5, source: 'http://www.github.com'});
+        return knex('links').insert({'user_id': 5, source: 'http://www.github.com'});
     })
     .then(function () {
-        knex('links').insert({'user_id': 5, source: 'http://www.codewars.com'});
+        return knex('links').insert({'user_id': 5, source: 'http://www.codewars.com'});
     })
     .then(function () {
-        knex('links').insert({'user_id': 5, source: 'http://www.linkedin.com'});
+        return knex('links').insert({'user_id': 5, source: 'http://www.linkedin.com'});
     })
     .then(function () {
-        knex('links').insert({'user_id': 6, source: 'http://www.github.com'});
+        return knex('links').insert({'user_id': 6, source: 'http://www.github.com'});
     })
     .then(function () {
-        knex('links').insert({'user_id': 6, source: 'http://www.codewars.com'});
+        return knex('links').insert({'user_id': 6, source: 'http://www.codewars.com'});
     })
     .then(function () {
-        knex('links').insert({'user_id': 6, source: 'http://www.linkedin.com'});
+        return knex('links').insert({'user_id': 6, source: 'http://www.linkedin.com'});
     })
     .catch(function(err) {
 	    console.log(`There was an error inserting into links table: ${err}`);
