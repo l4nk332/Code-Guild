1a7c4828e7fb651317a0859fbb311d1c17229d2b
Yeshe Wingerd <yeshito@gmail.com>
2016-06-27 19:02:54 -0700

    completed user login setup

--- routes/login.js
+++ routes/login.js
@@ -1,27 +1,30 @@
 var express = require('express');
 var router = express.Router();
-
+var bcrypt = require('bcrypt');
+var knex = require('../db/knex');
+var cookieParser = require('cookie-parser');
+var cookieSession = require('cookie-session');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('login.nunjucks', { title: 'CodeGuild' });
 });
-router.post('/', (req, res, next) => {
-    knex('users').select().where( 'username', req.body.username ).then((user) => {
-        if (user) {
-          console.log('user is: ' + user)
-            var hash = bcrypt.hashSync(req.body.password, 8)
-            if (bcrypt.compareSync(req.body.password, user[0].user_password)) {
-                req.session.user = user[0].user_id;
-                res.redirect('/user');
+
+router.post('/', function (req, res, next) {
+    knex('users').select().where('username', req.body.username ).then(function (user) {
+        if (user.length !== 0) {
+            var hash = bcrypt.hashSync(req.body.password, 8);
+            if (bcrypt.compareSync(req.body.password, user[0].password)) {
+                req.session.user = user[0].id;
+                console.log('password matched!')
+                res.redirect('/users');
             } else {
-              // res.render('signin', error: "Email/password don't match");
+              res.render('login.nunjucks', { error: "Email/password don't match" });
               console.log('passwords do not match yo')
             }
         } else {
-          // res.render('signin', error: "Email/password don't match");
+          res.render('login.nunjucks', { error: "Email/password don't match" });
           console.log('username not found yo')
           }
-        res.redirect('/');
     })
 });
 module.exports = router;
