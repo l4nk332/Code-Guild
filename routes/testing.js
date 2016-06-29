d898c4c03067ba90a06c4b7b080bd0dd335b53d1
Yeshe Wingerd <yeshito@gmail.com>
2016-06-29 13:25:39 -0700

    front end part of profile edit page.

--- routes/testing.js
+++ routes/testing.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('testing.nunjucks', { title: 'Code Guild' });
+  res.render('profile.nunjucks', { title: 'Code Guild' });
+  // res.render('testing.nunjucks', { title: 'Code Guild' });
 });
 
 module.exports = router;
