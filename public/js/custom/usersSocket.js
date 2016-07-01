e1f09180d042a05b1c867d2e70a51a39f5d83876
Yeshe Wingerd <yeshito@gmail.com>
2016-06-30 21:36:36 -0700

    not much

--- public/js/custom/usersSocket.js
+++ public/js/custom/usersSocket.js
@@ -5,11 +5,11 @@ $( document ).ready(function() {
   socket.emit('user logged in', username);
 
   socket.on('session query', function (requesterName) {
-    console.log('requesterName is: ' + requesterName);
-    $('body').append(`<div class="sessionCard">${requesterName} is requesting a session with you</div>`)
+
+    $('body').append(`${requesterName.teacher}${requesterName.student}${requesterName.studentPhoto}${requesterName.sessionType} is requesting a session with you</div>`)
     $('.startSession').click(function(e) {
       // open new page in this teacher's browser
-      var sessionUsersString = username + requesterName;
+      var sessionUsersString = username + '/' + requesterName;
     })
     socket.emit('session initiated', sessionURL);
   })
@@ -18,22 +18,37 @@ $( document ).ready(function() {
     // open new page in this student's browser
   })
 
+  socket.on('status change', function(userStatusChange) {
+    var statusChangeUser = userStatusChange.username;
+    var userStatus = userStatusChange.status;
+    var cardToChange = $('.card').attr('data-teacherusername').val(loggedOutUser).closest('.status-container');
+
+    if (user.available) {
+      $(cardToChange).html("<span class='status-text'>Available</span><span class='status-available'></span>")
+    }
+    else {}
+      // <span class="status-text">Unavailable</span>
+      // <span class="status-unavailable"></span>
+  })
+
 
   $('.userStatus').click(function(e) {
     var userStatus = {username: username, status: e.target.val()};
     socket.emit('status change', userStatus);
   })
 
-  socket.on('status change', function(userAndStatus) {
-    var statusUser = userAndStatus.username;
-    var status = userAndStatus.status;
-    // code needed here to change status of specific user in the dom
-  })
+  // socket.on('status change', function(userAndStatus) {
+  //   var statusUser = userAndStatus.username;
+  //   var status = userAndStatus.status;
+  //   code needed here to change status of specific user in the dom
+  // })
 
-  $('.requestSession').click(function() {
-    var teacher = $(this).attr('id');
-    console.log('var teacher is: ' + teacher);
-      socket.emit('request session', {teacher: teacher, student: username});
+  $('.session-request').click(function() {
+    var teacher = $(this).closest('.card').attr('data-teacherusername');
+    var sessionType = $(this).text();
+    var studentPhoto = $('#avatar-dropdown').attr('src');
+
+      socket.emit('request session', {teacher: teacher, student: username, studentPhoto: studentPhoto, sessionType: sessionType});
   })
 
 
